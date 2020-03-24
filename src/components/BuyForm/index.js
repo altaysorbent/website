d3d15de69d1fec9f7c2b39b72482ee403e2c7136
Roman Sadoyan <sadoyan92@gmail.com>
2020-03-24 12:03:23 +0000

    Update index.js

--- src/components/BuyForm/index.js
+++ src/components/BuyForm/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import BuyFormManager from './BuyFormManager';
+import BuyFormManager from './BuyFormManager.js';
 import classNames from 'classnames';
 
 const BuyForm = () => {
