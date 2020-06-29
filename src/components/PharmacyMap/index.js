e1434fc34eb3553c5c7789ab9ff72b0a9dc771c5
Roman Sadoyan <sadoyan@protonmail.com>
2020-06-29 15:14:29 +0300

    move client outside of component

--- src/components/PharmacyMap/index.js
+++ src/components/PharmacyMap/index.js
@@ -101,15 +101,15 @@ const Map = props => {
     </>
   );
 };
+const client = contentful.createClient({
+  // This is the space ID. A space is like a project folder in Contentful terms
+  space: `57e4k2ca6fmc`,
+  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+  accessToken: `Lh4LbfbNBL_vd6Vy22vMWSmiPiVdhYwenpfNzfyAyjg`,
+});
 
 const PharmacyMap = () => {
   const [pharmacies, setPharmacies] = useState([]);
-  const client = contentful.createClient({
-    // This is the space ID. A space is like a project folder in Contentful terms
-    space: `57e4k2ca6fmc`,
-    // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-    accessToken: `Lh4LbfbNBL_vd6Vy22vMWSmiPiVdhYwenpfNzfyAyjg`,
-  });
 
   useEffect(() => {
     client
