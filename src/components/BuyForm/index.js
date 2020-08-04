ada8b772b875cfb85f77aa2c82772b4c47141f3a
amirqa17 <49957940+amirqa17@users.noreply.github.com>
2020-08-04 19:32:40 +0600

    Почтовый индекс ZIP

--- src/components/BuyForm/index.js
+++ src/components/BuyForm/index.js
@@ -397,7 +397,7 @@ const BuyForm = () => {
                     className:
                       'appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-500 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500',
                     type: 'text',
-                    placeholder: '070004',
+                    placeholder: 'Индекс',
                   }}
                   wrapperStyle={{ display: 'block' }}
                   items={zipcodes}
