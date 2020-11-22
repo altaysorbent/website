bc9ab4a8544e333c1e01ca16068763f409042a09
Roman Sadoyan <sadoyan@protonmail.com>
2020-11-22 15:07:49 +0300

    changes according to the new API

--- src/constants/product.js
+++ src/constants/product.js
@@ -2,12 +2,18 @@ const productName = 'Алтайсорбент 1г/20 шт.';
 const minimumAvailableCount = 1;
 const maximumAvailableCount = 64;
 const productPriceKzt = 630;
-const productPriceRub = 111;
+const productPriceRub = 113;
 const currencyCode = 'KZT';
+const defaultProductId = 20;
 
 const DELIVERY_TYPES = {
   WAREHOUSE: 136,
   DELIVERY: 137,
+};
+
+const SENDER_CITY_IDS = {
+  SPB: 137,
+  UKG: 11903,
 };
 
 const CURRENCY_SYMBOLS = {
@@ -24,4 +30,6 @@ export {
   currencyCode,
   DELIVERY_TYPES,
   CURRENCY_SYMBOLS,
+  SENDER_CITY_IDS,
+  defaultProductId,
 };
