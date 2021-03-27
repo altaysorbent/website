196f4166bcf9c433caec08d89a6e0a0adfd71984
Roman Sadoyan <sadoyan92@gmail.com>
2021-03-27 17:39:40 +0300

    add styles to post rich text

--- components/Post/PostRichText.tsx
+++ components/Post/PostRichText.tsx
@@ -1,7 +1,28 @@
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { BLOCKS } from '@contentful/rich-text-types';
+import styles from './PostRichText.module.scss';
+
+const options = {
+  renderNode: {
+    [BLOCKS.LIST_ITEM]: (node) => {
+      const UnTaggedChildren = documentToReactComponents(node, {
+        renderNode: {
+          [BLOCKS.PARAGRAPH]: (node, children) => children,
+          [BLOCKS.LIST_ITEM]: (node, children) => children,
+        },
+      });
+
+      return <li> {UnTaggedChildren} </li>;
+    },
+  },
+};
 
 const PostRichText = ({ json, className = '' }) => {
-  return <div className={className}>{documentToReactComponents(json)}</div>;
+  return (
+    <div className={[className, styles.richText].join(' ')}>
+      {documentToReactComponents(json, options)}
+    </div>
+  );
 };
 
 export default PostRichText;
