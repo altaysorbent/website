4f8916cf898d639176e114d8aa87ca5d5bdabb28
Roman Sadoyan <roman@sadoyan.me>
2024-04-03 15:56:41 +0300

    Update year

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = (): JSX.Element => {
       <div className="container mx-auto">
         <div className="w-full pt-6 text-center">
           <p className="text-white">
-            <a href="https://altaysorbent.org">Алтайсорбент</a> © 1994 - 2023
+            <a href="https://altaysorbent.org">Алтайсорбент</a> © 1994 - 2024
           </p>
           <div className="text-white mt-4">
             <a
