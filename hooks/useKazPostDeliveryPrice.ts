246cab9d4dc1a96d5dd244659ecd9a0d6b15efc8
Roman Sadoyan <roman@sadoyan.me>
2024-07-18 22:37:50 +0300

    Update next.js / update cdek api

--- hooks/useKazPostDeliveryPrice.ts
+++
@@ -1,9 +0,0 @@
-import { KazPostDeliveryPrice } from 'constants/Product';
-
-export const useKazPostDeliveryPrice = (count: number): number[] => {
-  const multiplier = Math.round(count / 2);
-  const priceKzt = KazPostDeliveryPrice.KZT;
-  const priceRub = KazPostDeliveryPrice.RUB;
-
-  return [multiplier * priceKzt, multiplier * priceRub];
-};
