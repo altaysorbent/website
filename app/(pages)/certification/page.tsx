7dc3b3e2bcd1377f522c208b6093452a6600cb2b
Roman Sadoyan <roman@sadoyan.me>
2024-07-20 22:31:46 +0300

    Apply next/image fixes

--- app/(pages)/certification/page.tsx
+++ app/(pages)/certification/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import type { Metadata } from 'next';
 import { getMeta } from '@/lib/meta';
+import Image from 'next/image';
 
 const title = 'Сертификация';
 export const metadata: Metadata = getMeta({ title });
@@ -17,42 +18,54 @@ const CertificationPage = (): React.JSX.Element => {
           документов для ознакомления с нашей компанией и продукцией.
         </p>
         <div className="mb-12 flex items-center justify-center">
-          <div className="w-1/3 px-3">
+          <div className="relative w-1/3 px-3">
             <a
               className="cursor-pointer"
               href="/images/certificates/sertificate-eas.jpg"
               rel="noopener noreferrer"
               target="_blank"
             >
-              <img
+              <Image
+                width={0}
+                height={0}
+                sizes="100vw"
+                style={{ width: '100%', height: 'auto' }}
                 alt="Свидетельство о регистрации"
                 src="/images/certificates/sertificate-eas.jpg"
               />
             </a>
           </div>
-          <div className="w-1/3 px-3">
+          <div className="relative w-1/3 px-3">
             <a
               className="cursor-pointer"
               href="/images/certificates/test-report.jpg"
               rel="noopener noreferrer"
               target="_blank"
             >
-              <img
+              <Image
+                width={0}
+                height={0}
+                sizes="100vw"
+                style={{ width: '100%', height: 'auto' }}
                 alt="Протокол испытаний"
                 src="/images/certificates/test-report.jpg"
               />
             </a>
           </div>
-          <div className="w-1/3 px-3">
+          <div className="relative w-1/3 px-3">
             <a
               className="cursor-pointer"
               href="/images/certificates/test-report-2.jpg"
               rel="noopener noreferrer"
               target="_blank"
             >
-              <img
-                alt="Протокол испытаний"
+              <Image
+                width={0}
+                height={0}
+                sizes="100vw"
+                style={{ width: '100%', height: 'auto' }}
                 src="/images/certificates/test-report-2.jpg"
+                alt="Протокол испытаний"
               />
             </a>
           </div>
